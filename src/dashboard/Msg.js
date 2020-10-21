60ed96560709c6cd1fe154e2ca3aa3f9ca128bbc
jawish-a@outlook.com <jawish-a@outlook.com>
2020-10-22 00:15:14 +0300

    imgs and urls in msgs are working

--- src/dashboard/Msg.js
+++ src/dashboard/Msg.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-function Msg({msg}) {
-  
+const Msg = ({ msg }) => {
+
+  const urlRegex = /[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/gi;
+  // const urlRegex = new RegExp('/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi')
+  const imgRegex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/gi;
+  let msgBody
+
+  if (msg.message.match(imgRegex)) {
+    // msgBody = <a href={msg.message} target="_blank">{msg.message}</a>
+    msgBody = <img src={msg.message} alt=""/>
+  }
+  else if (msg.message.match(urlRegex)){
+    if (msg.message.match(urlRegex)) {
+      msgBody = <a href={msg.message} target="_blank">{msg.message}</a>
+    }
+  }
+  else {
+    msgBody = <p>{msg.message}</p>
+  }
+
   // chanel_id, id, msg, timestamp, username
   return (
     <div className="container border border-primary my-3">
@@ -9,7 +27,7 @@ function Msg({msg}) {
       <div className="container">
         <div className="container row">
           <div className="col-2">{msg.username}</div>
-          <div className="col-10">{msg.message}</div>
+          <div className="col-10">{msgBody}</div>
           <p>{msg.timestamp}</p>
         </div>
       </div>
